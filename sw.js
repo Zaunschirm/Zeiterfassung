da3c5d20aa80828768bf8275c170a16de755e88b
Zaunschirm <zaunschirms@gmail.com>
2025-10-01 05:41:23 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,13 @@
-const CACHE = 'zeit-pwa-v2';
-const ASSETS = ['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE = 'zeit-pwa-v4';
+const ASSETS = ['./','./index.html','./users.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(self.clients.claim()); });
-self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(resp => { const copy=resp.clone(); caches.open(CACHE).then(c=>c.put(e.request, copy)); return resp; }).catch(()=>caches.match('./index.html')))); });
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request).then(r => {
+      const copy = r.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy));
+      return r;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
