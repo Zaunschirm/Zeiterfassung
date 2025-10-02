c15e6194e3bbc272d2406e0f0e8f09ffdc020038
Zaunschirm <zaunschirms@gmail.com>
2025-10-02 05:21:44 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'zeit-pwa-v8-zaunschirm';
+const CACHE = 'zeit-pwa-v9-zaunschirm';
 const ASSETS = ['./','./index.html','./users.html','./config.json','./assets/logo.png','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(self.clients.claim()); });
