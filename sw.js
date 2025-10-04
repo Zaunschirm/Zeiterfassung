0032c68ff518814a9d330a771946e5e12ee99163
Zaunschirm <zaunschirms@gmail.com>
2025-10-04 05:47:56 +0200

    v20

--- sw.js
+++ sw.js
@@ -1,1 +1,4 @@
-const CACHE='zeit-pwa-v19';self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./config.json','./manifest.json','./assets/logo.png','users/','users/index.html'])));self.skipWaiting();});self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim());});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
+const CACHE='zeit-pwa-v20';
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./config.json','./manifest.json','./assets/logo.png','./icons/icon-192.png','./icons/icon-512.png','users/','users/index.html'])));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim());});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
