4e0eafb16be1476b5001dcd956bc0c33f6e4e950
Zaunschirm <zaunschirms@gmail.com>
2025-10-05 12:40:02 +0200

    2

--- sw.js
+++ sw.js
@@ -1,2 +1,3 @@
 self.addEventListener('install', e=>{ self.skipWaiting(); }); self.addEventListener('activate', e=>{}); self.addEventListener('fetch', e=>{});
 // V01e 2025-10-05T09:27:21.076012Z
+// V01f UI lift
