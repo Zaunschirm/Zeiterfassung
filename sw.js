b9c337b3e4c01d8e57684cd6fab461d501c42800
Zaunschirm <zaunschirms@gmail.com>
2025-10-05 06:59:34 +0200

    r

--- sw.js
+++ sw.js
@@ -1,4 +1,17 @@
-const CACHE='zeit2-pwa-v58';
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./assets/logo.png','./users/'])))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))) });
-self.addEventListener('fetch',e=>{ if(e.request.method!=='GET')return; e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const cp=res.clone(); caches.open(CACHE).then(c=>c.put(e.request,cp)); return res;}).catch(()=>r))) });
+// V59
+const CACHE='zeit2-pwa-v59';
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./assets/logo.png','./users/'])));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+      const copy=res.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy));
+      return res;
+    }).catch(()=>r))
+  );
+});
