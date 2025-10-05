6e86a81fceb6833499ea74cc504f4741ffbe13ac
Zaunschirm <zaunschirms@gmail.com>
2025-10-05 05:13:54 +0200

    e

--- sw.js
+++ sw.js
@@ -1,4 +1,18 @@
-const CACHE='zeit-pwa-v50';
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./config.json','./manifest.json','./assets/logo.png','./icons/icon-192.png','./icons/icon-512.png','users/','users/index.html'])));self.skipWaiting();});
-self.addEventListener('activate', e => { e.waitUntil((async ()=>{ const keys=await caches.keys(); await Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))); await self.clients.claim(); })()); });
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
+const CACHE='zeit-pwa-v51';
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./assets/logo.png','./users/'])));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+});
+self.addEventListener('fetch',e=>{
+  const url=new URL(e.request.url);
+  if(e.request.method!=='GET') return;
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+      const copy=res.clone();
+      caches.open(CACHE).then(c=>c.put(e.request,copy));
+      return res;
+    }).catch(()=>r))
+  );
+});
