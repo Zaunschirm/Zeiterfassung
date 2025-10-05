6a287169a57a5a4e8ebaf67e97711458b7388a1c
Zaunschirm <zaunschirms@gmail.com>
2025-10-05 07:38:06 +0200

    2

--- sw.js
+++ sw.js
@@ -1,1 +1,10 @@
-const CACHE='zeit-pwa-v61'; self.addEventListener('install',()=>{});
+const CACHE='zeit-pwa-v61';
+const ASSETS=['./','./index.html','./manifest.json','./assets/logo.png','./users/'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))))});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+    const copy=res.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return res;
+  }).catch(()=>r)));
+});
