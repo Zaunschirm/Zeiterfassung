353539f607bf6ddb2519d3c0794106eeecb5aea2
Zaunschirm <zaunschirms@gmail.com>
2025-10-20 06:02:35 +0200

    1

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
+export default ({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
-  const base = env.VITE_BASE || '/Zeiterfassung/'
-  return {
-    base,
+  return defineConfig({
     plugins: [react()],
-    server: { port: 5173, host: true },
-    build: { outDir: 'dist' }
-  }
-})
+    base: env.VITE_BASE || '/',   // auf Vercel ist das '/'
+  })
+}
