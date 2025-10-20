eea4b10c076d3a08c21e3a57ec6f9ebcef4d88d6
Zaunschirm <zaunschirms@gmail.com>
2025-10-20 06:13:19 +0200

    2

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,8 @@
-
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import './styles.css'
-const root = createRoot(document.getElementById('root'))
-root.render(<App />)
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/src/service-worker.js').catch(console.error)
+  import('virtual:pwa-register').then(({ registerSW }) => {
+    registerSW({
+      onNeedRefresh() {},
+      onOfflineReady() {}
+    })
   })
 }
